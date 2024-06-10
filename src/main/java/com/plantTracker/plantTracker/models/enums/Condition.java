ea49c209f7cf0aadeb8566aa7b8f616ccdc5b8d6
Rabin Kandel <kandelrabin@live.com>
2024-06-10 14:39:03 +0100

    add Climate enum

---
+++ src/main/java/com/plantTracker/plantTracker/models/enums/Condition.java
@@ -0,0 +1,50 @@
+package com.plantTracker.plantTracker.models.enums;
+
+public class Condition {
+    private double minTemperature;
+    private double maxTemperature;
+    private double minHumidity;
+    private double maxHumidity;
+
+    public Condition(double minTemperature, double maxTemperature, double minHumidity, double maxHumidity) {
+        this.minTemperature = minTemperature;
+        this.maxTemperature = maxTemperature;
+        this.minHumidity = minHumidity;
+        this.maxHumidity = maxHumidity;
+    }
+
+    public Condition() {
+    }
+
+    public double getMinTemperature() {
+        return minTemperature;
+    }
+
+    public void setMinTemperature(double minTemperature) {
+        this.minTemperature = minTemperature;
+    }
+
+    public double getMaxTemperature() {
+        return maxTemperature;
+    }
+
+    public void setMaxTemperature(double maxTemperature) {
+        this.maxTemperature = maxTemperature;
+    }
+
+    public double getMinHumidity() {
+        return minHumidity;
+    }
+
+    public void setMinHumidity(double minHumidity) {
+        this.minHumidity = minHumidity;
+    }
+
+    public double getMaxHumidity() {
+        return maxHumidity;
+    }
+
+    public void setMaxHumidity(double maxHumidity) {
+        this.maxHumidity = maxHumidity;
+    }
+}
